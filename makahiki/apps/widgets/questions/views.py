05fea7a2b9d8929d2e7c611b705cb581c2f77cd5
Justin <justin.t.weaver@gmail.com>
2013-04-26 23:08:20 -1000

    Developed view for questions widget; modified questions widget template

--- makahiki/apps/widgets/questions/views.py
+++ makahiki/apps/widgets/questions/views.py
@@ -1,8 +1,29 @@
 """Provides the view of the widget."""
-
+from apps.widgets.questions.models import *
 
 def supply(request, page_name):
     """ supply view_objects for widget rendering."""
     _ = request
     _ = page_name
-    return {}
+    
+    questions = []
+    
+    for question in Question.objects.all():
+        q = {}
+        q['question'] = question.text
+        q['type'] = question.type
+        
+        if question.type == Question.SINGLE:
+            pass
+        elif question.type == Question.MULTI:
+            pass
+        elif question.type == Question.MATCH:
+            pass
+        
+        questions.append(q)
+    
+    print questions
+        
+    return {
+        "questions": questions,
+    }
